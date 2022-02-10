476a423a94ea061bce402e417e9c04d408609e34
David Donohue <dpdonohue9427@gmail.com>
2022-02-10 16:50:06 -0500

    More than I can say right now but basically a lot

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
+import os
 import sys
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
 import requests
+import json
+
+
+# Initial function to prompt user for dl info
+def get_user_info():
+    f = open('archiveurls.json')
+    data = json.load(f)
+
+    print("Select the archive you want to pull from")
+    print("0) MANUAL LINK")
+
+    cnt = 1
+    for i in data['ArchiveList']:
+        name = i["console"]
+        consoles.append(name)
+        print("{}) {}".format(cnt, name))
+        cnt = cnt+1
+
+    print("Please make a selection above:")
+
+    global host_site
+    global usr_str
+    host_sel = int(input())
+
+    if host_sel == 0:
+        host_site.append(input())
+    else:
+        for i in data["ArchiveList"]:
+            if i["console"] == consoles[int(host_sel) - 1]:
+                for j in i["link"]:
+                    host_site.append(j)
+                    print(j)
+
+    print("Enter a string to filter results:")
+    usr_str = input()
+
+    global usr_path
+
+    print("Choose a directory to save the files (Type here to choose cur dir):")
+    print(os.getcwd() + "to:")
+    os.chdir(input())
+    print("new path:" + os.getcwd())
+
+
+
+
 
 
 # loop through links on page
-
-
-def get_links():
-    req = Request(host_site)
+def get_links(i):
+    req = Request(i)
     html_page = urlopen(req)
     soup = BeautifulSoup(html_page, "lxml")
 
@@ -19,17 +64,56 @@ def get_links():
 
     for url in links:
         if url is not None and url.find(usr_str) != -1 and url.find("/") == -1:
-            usr_links.append(url)
-
-    print("===============================================================================")
+            usr_links.append(i + "/" + url)
 
 
 # Calculate file size from url
 def calc_space(url):
     response = requests.head(url, allow_redirects=True)
-    size = response.headers.get('content-length', -1)
-    print(size)
+    size = response.headers.get("content-length", -1)
+    print(".", end="")
     return int(size)
+
+
+# Get the total count and size of files and display for conformation
+def loop_space():
+    space = 0
+    for link_name in usr_links:
+        space += calc_space(link_name)
+
+    print("\n{}: {}".format("Total number of files selected ", len(usr_links)))
+
+    if int(space) / KBFACTOR < 1042:
+        print("{}: {:.2f} KB".format("Total size of selected files ", int(space) / KBFACTOR))
+    elif int(space) / MBFACTOR < 1024:
+        print("{}: {:.2f} MB".format("Total size of selected files ", int(space) / MBFACTOR))
+    else:
+        print("{}: {:.2f} GB".format("Total size of selected files ", int(space) / GBFACTOR))
+    print("Do you want you continue? Y/N")
+
+    global continue_dl
+    while continue_dl == 0:
+        confirm = input()
+        if confirm == "y" or confirm == "Y":
+            print("Starting download of files.")
+            loop_download()
+            continue_dl = 1
+        elif confirm == "n" or confirm == "N":
+            print("Download cancelled..")
+            continue_dl = 2
+        else:
+            print("Sorry, huh?")
+
+
+# loop through the list of links and download each one individually.
+def loop_download():
+    for link_name in usr_links:
+        download(link_name,
+                 link_name.replace("%20", " ").replace("%21", "!").replace("%28", "(").replace("%29", ")").replace(
+                     "%2C", ",").replace("%2D", "-").replace("%5B", "[").replace("%5D", "]"))
+
+    print("===============================================================================")
+    print("Downloads Complete")
 
 
 # Download file from url
@@ -57,94 +141,27 @@ def download(url, filename):
     print("\n")
 
 
-# Call downloads for each link in list
-def loop_links(flag):
-    if flag == 0:
-        space = 0
-        for link_name in usr_links:
-            space += calc_space(host_site + "/" + link_name)
-        print(space)
-        print("Do you want you continue? Y/N")
-        confirm = input()
-        if confirm == ("y" or "Y"):
-            print("Cool")
-        if confirm == ("n" or "N"):
-            print("Not so Cool")
-
-    if flag == 1:
-        for link_name in usr_links:
-            download(host_site + "/" + link_name,
-                     link_name.replace("%20", " ").replace("%21", "!").replace("%28", "(").replace("%29", ")").replace(
-                         "%2C", ",").replace("%2D", "-").replace("%5B", "[").replace("%5D", "]"))
-
-        print("===============================================================================")
-        print("Downloads Complete")
-
-
-# Initial function to prompt user for dl info
-def get_user_info():
-    print("Select the archive you want to pull from")
-    print("0) MANUAL LINK")
-    print("1) OG Gameboy")
-    print("2) Gameboy Color")
-    print("3) Gameboy Advance")
-    print("4) NES")
-    print("5) SNES")
-    print("6) Nintendo64")
-    print("7) NYF-Gamecube")
-    print("8) NYF-Wii")
-    print("9) NYF-WiiU")
-    print("10) Nintendo DS")
-    print("11) DS Download Play")
-    print("12) DSi")
-    print("13) DSi Ware")
-    print("14) NYF-Nintendo 3DS")
-    print("Please make a selection above:")
-
-    global host_site
-    global usr_str
-    host_sel = int(input())
-
-    if host_sel == 0:
-        host_site = input()
-    else:
-        if host_sel != 7 and host_sel != 8 and host_sel != 9 and host_sel != 14 and host_sel < len(archives):
-            host_site = archives[host_sel - 1]
-            print(host_sel)
-            print(host_site)
-
-    print("Enter a string to filter results:")
-    usr_str = input()
-
-
+KBFACTOR = float(1 << 10)
+MBFACTOR = float(1 << 20)
+GBFACTOR = float(1 << 30)
+continue_dl = 0;
+consoles = []
 usr_links = []
+host_site = []
 usr_str = ""
-host_site = ""
+usr_path = ""
 
-archives = ["https://archive.org/download/nointro.gb",
-            "https://archive.org/download/nointro.gbc",
-            "https://archive.org/download/nointro.gba",
-            "https://archive.org/download/nointro.nes",
-            "https://archive.org/download/nointro.snes",
-            "https://archive.org/download/nointro.n64",
-            "NA",
-            "NA",
-            "NA",
-            "https://archive.org/download/no-intro-nintendo-nintendo-ds-decrypted",
-            "https://archive.org/download/no-intro-nintendo-nintendo-ds-download-play",
-            "https://archive.org/download/no-intro-nintendo-nintendo-dsi-decrypted",
-            "https://archive.org/download/no-intro-nintendo-nintendo-dsi-digital",
-            "NA"
-            ]
-
-# start funtion
+# start function
 get_user_info()
 
 # get list of links from url containing filter string
-get_links()
+for src in host_site:
+    get_links(src)
+
 
 # calc space of download / confirm you want to proceed
-loop_links(0)
+loop_space()
 
-# start download from created and approved list
-# loop_links(1)
+# start downloading files or skip and end.
+if continue_dl == 1:
+    loop_download()
